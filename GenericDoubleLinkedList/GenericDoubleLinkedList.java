9ef81627c77aecd54b19cf475b3df540dcd5eb6c
garrettspillman1 <garrettspillman1@gmail.com>
2024-01-17 18:55:59 -0500

    All Data Structures JavaDoced

--- GenericDoubleLinkedList/GenericDoubleLinkedList.java
+++ GenericDoubleLinkedList/GenericDoubleLinkedList.java
@@ -1,152 +1,227 @@
+/**
+ * TODO: Not sure if this works may need to revist
+ * @garrettspillman1
+ * A generic doubly linked list implementation.
+ * This class allows for the manipulation of elements in a doubly linked list,
+ * providing methods for navigation, modification, and retrieval of data.
+ * 
+ * @param <T> The type of elements stored in the linked list.
+ */
 public class GenericDoubleLinkedList<T> {
-	// Doesn't work need to workshop it 
-	private class Node {
-		T data;
-		Node next;
-		Node prev;
-		
-		public Node(T data, Node next, Node prev ) {
-			this.data = data;
-			this.next = next;
-			this.prev = prev;
-		}
-	}
-	
-	private Node head;
-	private Node current;
-	private int size;
-	
-	public void gotoNext()
-	{
-		if (current != null) {
-	        current = current.next;
-	        
-	    }
-	}
-	public void goToPrev()
-	{
-		if (current != null && current.prev != null) {
-	        current = current.prev;
-	        
-	    }
-	}
-	public void reset() {
-		current = head;
-	}
-	public void gotoEnd() {
-	    if (head == null) {
-	        return;
-	    }
-	    current = head;
-	    while (current.next != null) {
-	        current = current.next;
-	    }
-	}
 
-	public boolean hasMore() {
-		    if (current == null) 
-		    {
-		        return false; 
-		    }
-		    return true;
-		}
-	
-	public T getCurrent() {
-	    if (current != null) {
-	        return current.data;
-	    }
-	    return null;
-	}
-	public void setCurrent(T data) {
-	    if (current != null) {
-	        current.data = data;
-	    }
-	}
-	public void add(T data) {
-	    Node temp1 = new Node(data, null, null);
-	    if (head == null) {
-	        // The list is empty
-	        head = current = temp1;
-	    } else {
-	        //go to the end and add the new node
-	        Node temp2 = head;
-	        while (temp2.next != null) {
-	            temp2 = temp2.next;
-	        }
-	        temp2.next = temp1;
-	        temp1.prev = temp2;
-	    }
-	    size++;
-	}
-	public void addAfterCurrent(T data) {
-	    if (current == null) {
-	        return; // Do not add the data to the list
-	    }
-	    Node temp = new Node(data, current.next, current);
-	    if (current.next != null) {
-	        current.next.prev = temp;
-	    }
-	    current.next = temp;
-	    size++;
-	    
-	}
-	public void remove(T data) {
-	    Node temp = head;
-	    while (temp != null && temp.data != data) {
-	        temp = temp.next;
-	    }
-	    if (temp == null) {
-	        return; // The data is not in the list
-	    }
-	    if (temp.prev == null) {
-	        head = temp.next;
-	    } else {
-	        temp.prev.next = temp.next;
-	    }
-	    if (temp.next != null) {
-	        temp.next.prev = temp.prev;
-	    }
-	    if (current == temp) {
-	        current = temp.next;
-	    }
-	    size--;
-	}
-	public void removeCurrent() {
-	    if (current == null) {
-	        return; // can't remove if the current reference is null
-	    }
-	    if (current.prev == null) {
-	        head = current.next;
-	    } else {
-	        current.prev.next = current.next;
-	    }
-	    if (current.next != null) {
-	        current.next.prev = current.prev;
-	        current = current.next;
-	    } else {
-	        current = current.prev;
-	    }
-	    size--;
-	}
-	public void print() {
-	    while (head != null) {
-	        System.out.println(head.data);
-	        head = head.next;
-	    }
-	    System.out.println();
-	}
-	public boolean contains(T data) {
-	    Node temp = head;
-	    while (temp != null) {
-	        if (temp.data == data) {
-	            return true;
-	        }
-	        temp = temp.next;
-	    }
-	    return false;
-	}
-	public int getSize() {
-	    return size;
-	}
-	
+    /**
+     * Inner class representing a node in the doubly linked list.
+     */
+    private class Node {
+        T data;
+        Node next;
+        Node prev;
+
+        /**
+         * Constructs a new node with the specified data, next node, and previous node.
+         *
+         * @param data The data to be stored in the node.
+         * @param next The next node in the list.
+         * @param prev The previous node in the list.
+         */
+        public Node(T data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node head;      // Reference to the head of the list
+    private Node current;   // Reference to the current position in the list
+    private int size;       // Number of elements in the list
+
+    /**
+     * Moves the current position to the next node in the list.
+     */
+    public void gotoNext() {
+        if (current != null) {
+            current = current.next;
+        }
+    }
+
+    /**
+     * Moves the current position to the previous node in the list.
+     */
+    public void goToPrev() {
+        if (current != null && current.prev != null) {
+            current = current.prev;
+        }
+    }
+
+    /**
+     * Resets the current position to the head of the list.
+     */
+    public void reset() {
+        current = head;
+    }
+
+    /**
+     * Moves the current position to the end of the list.
+     */
+    public void gotoEnd() {
+        if (head == null) {
+            return;
+        }
+        current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+    }
+
+    /**
+     * Checks if there are more nodes in the list after the current position.
+     *
+     * @return True if there are more nodes, false otherwise.
+     */
+    public boolean hasMore() {
+        return current != null;
+    }
+
+    /**
+     * Gets the data at the current position.
+     *
+     * @return The data at the current position, or null if the position is null.
+     */
+    public T getCurrent() {
+        return current != null ? current.data : null;
+    }
+
+    /**
+     * Sets the data at the current position.
+     *
+     * @param data The data to set.
+     */
+    public void setCurrent(T data) {
+        if (current != null) {
+            current.data = data;
+        }
+    }
+
+    /**
+     * Adds a new node with the specified data to the end of the list.
+     *
+     * @param data The data to be added.
+     */
+    public void add(T data) {
+        Node temp1 = new Node(data, null, null);
+        if (head == null) {
+            head = current = temp1;
+        } else {
+            Node temp2 = head;
+            while (temp2.next != null) {
+                temp2 = temp2.next;
+            }
+            temp2.next = temp1;
+            temp1.prev = temp2;
+        }
+        size++;
+    }
+
+    /**
+     * Adds a new node with the specified data after the current position.
+     *
+     * @param data The data to be added.
+     */
+    public void addAfterCurrent(T data) {
+        if (current == null) {
+            return;
+        }
+        Node temp = new Node(data, current.next, current);
+        if (current.next != null) {
+            current.next.prev = temp;
+        }
+        current.next = temp;
+        size++;
+    }
+
+    /**
+     * Removes the first occurrence of the specified data from the list.
+     *
+     * @param data The data to be removed.
+     */
+    public void remove(T data) {
+        Node temp = head;
+        while (temp != null && !temp.data.equals(data)) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            return;
+        }
+        if (temp.prev == null) {
+            head = temp.next;
+        } else {
+            temp.prev.next = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+        if (current == temp) {
+            current = temp.next;
+        }
+        size--;
+    }
+
+    /**
+     * Removes the node at the current position from the list.
+     */
+    public void removeCurrent() {
+        if (current == null) {
+            return;
+        }
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+        if (current.next != null) {
+            current.next.prev = current.prev;
+            current = current.next;
+        } else {
+            current = current.prev;
+        }
+        size--;
+    }
+
+    /**
+     * Prints the elements in the linked list.
+     */
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * Checks if the linked list contains the specified data.
+     *
+     * @param data The data to check for.
+     * @return True if the data is found, false otherwise.
+     */
+    public boolean contains(T data) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.equals(data)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    /**
+     * Gets the size of the linked list.
+     *
+     * @return The size of the linked list.
+     */
+    public int getSize() {
+        return size;
+    }
 }
--- Recursion/SimpleRecursion.java
+++ Recursion/SimpleRecursion.java
@@ -1,43 +1,69 @@
 package Recursion;
 
-
+/**
+ * @garrettspillman1
+ *                   This class provides examples of simple recursive methods.
+ */
 public class SimpleRecursion {
-	
-	
-	public static void main(String[] args) 
-	{
-	
+
+	/**
+	 * The main method (not used in this class).
+	 *
+	 * @param args Command line arguments (not used).
+	 */
+	public static void main(String[] args) {
+		// This method intentionally left blank.
 	}
-	public static void countDown(int i)
-	
-	{
-		if(i < 0)
+
+	/**
+	 * Recursively counts down from a given number to 0 and prints each count.
+	 *
+	 * @param i The starting number for the countdown.
+	 */
+	public static void countDown(int i) {
+		if (i < 0)
 			return;
 		System.out.println(i);
-		countDown(i-1);
-		
+		countDown(i - 1);
 	}
-	public static int factorial (int number)
-	{
-		if(number <= 0)
+
+	/**
+	 * Recursively calculates the factorial of a given number.
+	 *
+	 * @param number The number for which to calculate the factorial.
+	 * @return The factorial of the given number.
+	 */
+	public static int factorial(int number) {
+		if (number <= 0)
 			return 1;
 		else
-			return number * factorial(number-1);
-		
+			return number * factorial(number - 1);
 	}
-	public static int gcd ( int a, int b)
-	{
-		if(b == 0)
+
+	/**
+	 * Recursively calculates the greatest common divisor (GCD) of two numbers.
+	 *
+	 * @param a The first number.
+	 * @param b The second number.
+	 * @return The GCD of the two numbers.
+	 */
+	public static int gcd(int a, int b) {
+		if (b == 0)
 			return a;
-		else 
-			return gcd(b,a%b);
+		else
+			return gcd(b, a % b);
 	}
-	public static int fibonacci(int number)
-	{
-		if(number == 1 || number == 2)
+
+	/**
+	 * Recursively calculates the Fibonacci sequence value at a given position.
+	 *
+	 * @param number The position in the Fibonacci sequence.
+	 * @return The Fibonacci value at the given position.
+	 */
+	public static int fibonacci(int number) {
+		if (number == 1 || number == 2)
 			return 1;
 		else
-			return fibonacci(number-1)+fibonacci(number-2);
+			return fibonacci(number - 1) + fibonacci(number - 2);
 	}
-	
 }
